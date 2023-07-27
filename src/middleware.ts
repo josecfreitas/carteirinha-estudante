f6f5ba39cf22286a65f282a42a244f56bde71d1c
José Carlos de Freitas <josecfreitas@outlook.com.br>
2023-07-26 22:45:34 -0300

    add basic auth

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization')
+  const url = req.nextUrl
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [username, password] = atob(authValue).split(':')
+
+    const validUsername = process.env.BASIC_AUTH_USER
+    const validPassword = process.env.BASIC_AUTH_PASSWORD
+
+    if (username === validUsername && password === validPassword) {
+      return NextResponse.next()
+    }
+  }
+
+  url.pathname = '/api/auth'
+
+  return NextResponse.rewrite(url)
+}
