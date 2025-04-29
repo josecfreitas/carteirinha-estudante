4e33ce8f52e35257337b539633b1c6412e619391
José Carlos de Freitas <josecfreitas@outlook.com.br>
2025-04-29 19:56:01 -0300

    feat: add student profile pages and validation logic with QR code support

---
+++ src/app/[slug]/validacao/page.tsx
@@ -0,0 +1,56 @@
+import { Carteirinha, StudentSlug } from "@/components/carteirinha";
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
+import { PageProps } from "@/lib/page-props";
+
+// no-cache
+export const revalidate = 0;
+
+export default async function OthersValidation({
+  params,
+  searchParams,
+}: PageProps<{ slug: StudentSlug }>) {
+  const { slug } = await params;
+  const { "codigo-verificacao": codigoVerificacao } =
+    (await searchParams) || {};
+  const isValid =
+    codigoVerificacao === `${slug}-dfed9fff-8ee6-451e-830c-70s604g68254`;
+
+  return (
+    <main className="px-4">
+      <h1 className="my-6 text-center text-2xl font-bold text-[#073f60]">
+        Carterinha de Estudante
+      </h1>
+
+      {isValid ? (
+        <Carteirinha studentSlug={slug} cloaked />
+      ) : (
+        <p className="text-center">Carteirinha de estudante não encontrada!</p>
+      )}
+
+      <div className="text-center">
+        <h2 className="mt-8 mb-4 text-xl font-bold text-[#073f60]">
+          Validação
+        </h2>
+        {isValid ? (
+          <>
+            <FaCheckCircle size="70" className="m-auto fill-green-600" />
+            <p className="mt-2 mb-4">
+              <strong>Essa Carteirinha de Estudante é válida!</strong>
+            </p>
+          </>
+        ) : (
+          <>
+            <FaTimesCircle size="70" className="m-auto fill-red-600" />
+            <p className="mt-2 mb-4">
+              <strong>Essa Carteirinha de Estudante é inválida!</strong>
+            </p>
+          </>
+        )}
+        <p className="m-auto w-fit rounded-lg border border-gray-300 p-4">
+          Código de Verificação:{" "}
+          <span className="italic">{codigoVerificacao}</span>
+        </p>
+      </div>
+    </main>
+  );
+}
--- src/components/carteirinha.tsx
+++ src/components/carteirinha.tsx
@@ -3,11 +3,67 @@ import Image from "next/image";
 import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
+export type StudentSlug = keyof typeof students;
+
+const students = {
+  felipe: {
+    name: "Felipe Dias Ribeiro",
+    cpf: "386.189.978-70",
+    birthDate: "10/09/1990",
+    course: "Segurança da Informação",
+    semester: "8º",
+    shift: "Noturno",
+    registration: "12377718",
+  },
+  jose: {
+    name: "José Carlos de Freitas",
+    cpf: "423.734.998-51",
+    birthDate: "18/07/1995",
+    course: "Pós-Graduação - MBA em Arquitetura Full Cycle",
+    semester: "Pós",
+    shift: "Livre",
+    registration: "12326518",
+  },
+  willy: {
+    name: "Willy Domingues Martinez ",
+    cpf: "364.871.358-28 ",
+    birthDate: "10/05/1991",
+    course: "Pós-Graduação - Atendimento Educacional Especializado",
+    semester: "Pós",
+    shift: "Livre",
+    registration: "12366618",
+  },
+};
+
 interface CarteirinhaProps {
+  studentSlug?: StudentSlug;
   cloaked?: boolean;
 }
 
-export const Carteirinha = ({ cloaked }: CarteirinhaProps) => {
+export const Carteirinha = ({
+  cloaked,
+  studentSlug = "jose",
+}: CarteirinhaProps) => {
+  if (!students[studentSlug]) {
+    return (
+      <div className="m-auto block w-[550px] rounded-lg border border-gray-200 bg-white p-6 shadow">
+        <h2 className="text-center text-xl font-bold text-[#073f60]">
+          Estudante não encontrado
+        </h2>
+      </div>
+    );
+  }
+
+  const student = JSON.parse(JSON.stringify(students[studentSlug]));
+  if (cloaked) {
+    student.cpf = `${student.cpf.slice(0, 2)}*.***.***-${student.cpf.slice(-1)}`;
+    student.birthDate = "**/**/****";
+    student.registration = `${student.registration.slice(0, 2)}${Array(
+      student.registration.length - 4,
+    )
+      .fill("*")
+      .join("")}${student.registration.slice(-2)}`;
+  }
   return (
     <div className="m-auto block w-[550px] rounded-lg border border-gray-200 bg-white p-6 shadow">
       <div className="flex items-center justify-between">
@@ -30,11 +86,11 @@ export const Carteirinha = ({ cloaked }: CarteirinhaProps) => {
       <div className="my-6 flex gap-6">
         <div>
           <Image
-            src="/profile.jpg"
+            src={`/profile-${studentSlug}.jpg`}
             className="border border-[#073f60]"
             width={150}
             height={200}
-            alt="Logo da Faculdade de Brasília"
+            alt="Foto de perfil do estudante"
           />
 
           <div className="mt-1 text-center text-xs text-gray-100">
@@ -44,25 +100,15 @@ export const Carteirinha = ({ cloaked }: CarteirinhaProps) => {
         <ul className="grid grid-cols-2 gap-2">
           <Info
             label="Nome Completo"
-            value="José Carlos de Freitas"
+            value={student.name}
             className="col-span-2"
           />
-          <Info
-            label="CPF"
-            value={cloaked ? "42*.***.***-*1" : "423.734.998-51"}
-          />
-          <Info
-            label="Data de Nascimento"
-            value={cloaked ? "**/**/****" : "18/07/1995"}
-          />
-          <Info
-            label="Curso"
-            value="Pós-Graduação - MBA em Arquitetura Full Cycle"
-            className="col-span-2"
-          />
-          <Info label="Semestre" value="Pós" />
-          <Info label="Turno" value="Livre" />
-          <Info label="Matrícula" value={cloaked ? "12****18" : "12326518"} />
+          <Info label="CPF" value={student.cpf} />
+          <Info label="Data de Nascimento" value={student.birthDate} />
+          <Info label="Curso" value={student.course} className="col-span-2" />
+          <Info label="Semestre" value={student.semester} />
+          <Info label="Turno" value={student.shift} />
+          <Info label="Matrícula" value={student.registration} />
           <Info
             label="Validade"
             value={format(
